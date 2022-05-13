4b98b1b07f910ad0691a9bd05d1742ce55b46c08
junpatatous <mukdol1127@naver.com>
2022-05-13 21:39:57 +0900

    5/13 TIL

---
+++ JS/calculation-3.js
@@ -0,0 +1,35 @@
+/*
+console.log("ZA" > "AAAA");
+
+console.log("Hello" < "Hi");
+
+console.log("Hello" >= "Helloo");
+
+console.log("5" <= 10);
+
+console.log(true == 1);
+
+console.log(false != 123);
+
+console.log(true === 1);
+
+console.log(false !== 123);
+*/
+
+console.log(true || false);
+
+console.log(Boolean(0 || false));
+
+console.log(Boolean(123 || false));
+
+console.log(Boolean(123 && 0));
+
+console.log(Boolean(false && true));
+
+console.log(Boolean(true && 3));
+
+console.log(Boolean(0 && false));
+
+console.log(!false);
+
+console.log(!123);
---
+++ JS/scope.js
@@ -0,0 +1,48 @@
+/*
+let x = 1;
+let y = 2;
+let z = 3;
+
+console.log(x); 
+console.log(y); 
+{
+  
+  let x = 3;
+  let y = 4;
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
+
+
+console.log(x);
+console.log(y);
+*/
+ /*
+let A = 1;
+let B = 2;
+
+{
+  let C = 3;
+  let D = 4;
+
+  console.log(A);
+  console.log(C);
+}
+
+console.log(C);
+*/
+
+let A = 1;
+
+{
+  let C = 3;
+  let D = 4;
+  console.log(C);
+  {
+    let C = 5;
+    let D = 6;
+    console.log(C);
+
+  }
+}
