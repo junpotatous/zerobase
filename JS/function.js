896084671ada0c7ad03a7fbe9610ae115b68cd5b
junpatatous <mukdol1127@naver.com>
2022-05-16 22:57:51 +0900

    5/16 TIL

---
+++ JS/function.js
@@ -0,0 +1,25 @@
+/*
+function print_min() {
+  console.log(arguments[0]-arguments[1]);
+
+}
+
+print_min(10,20,30);
+*/
+
+/*
+function add(x,y) {
+  return x+y;
+}
+
+let result = add(10,20);
+console.log(result)
+*/
+
+function checkAge(age) {
+  if (age >= 18) return true;
+  else return false;
+}
+
+console.log(checkAge(14));
+console.log(checkAge(20));
