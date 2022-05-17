a132a6197db6c5590131bd7a2c7a6a707f9b1309
junpatatous <mukdol1127@naver.com>
2022-05-17 21:34:48 +0900

    5/17 TIL

---
+++ JS/function-practic.js
@@ -0,0 +1,8 @@
+function MAX(x,y) {
+  return x > y ? x:y
+
+}
+
+console.log(MAX(0,3));
+console.log(MAX(-1,5));
+console.log(MAX(100,7));
---
+++ JS/method3.js
@@ -0,0 +1,15 @@
+let user = { name: "jun"};
+let admin = {name: "admin"};
+
+function hello_func() {
+  console.log("Hello"+this.name);
+}
+
+user.func = hello_func;
+admin.func = hello_func;
+
+user.func();
+admin.func();
+
+user["func"]();
+admin["func"]();
