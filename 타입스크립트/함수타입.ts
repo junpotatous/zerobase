19237b40ef82c4c2c11e7f91bf15474bcc7fcadd
최준호 <103482590+junpotatous@users.noreply.github.com>
2022-07-08 14:30:26 +0900

    7/8 ts TIL

--- 타입스크립트/함수타입.ts
+++ 타입스크립트/함수타입.ts
@@ -4,27 +4,36 @@
  * 1. 매개변수
  * 2. 반환
  */
-
- function func(num: number, str: string):string {
+ function func(num: number, str: string): string {
   return num + str
-  //void는 반환하지않는다
-
 }
 
 func(123, 'str')
 
-
-function func2(num1: number, num2: string):number {
+function func2(num1: number, num2: string) {
   return num1 + Number(num2)
-
 }
 
 func2(123, '123')
 
 
-function func3(num1: number, num2: string):void {
+function func3(num1: number, num2: string): void {
   console.log(num1 + num2)
-
 }
 
-func3(123, '123') 
+func3(123, '123')
+
+
+const func4 = (str1: string, str2: string) => {
+  return str1 + ' ' + str2
+}
+
+
+func4('hello', 'world')
+
+const func5 = (obj: { str1: string, str2: string }) => {
+  return obj.str1 + ' ' + obj.str2
+}
+
+
+func5({ str1: 'hello', str2: 'world' })
